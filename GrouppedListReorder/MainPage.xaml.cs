47748eec967817b0cced8c830c3020f5bc02e490
Alexey Strakh <alexey.strakh@microsoft.com>
2021-02-16 15:38:34 -0800

    added community toolkit

--- GrouppedListReorder/MainPage.xaml.cs
+++ GrouppedListReorder/MainPage.xaml.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GrouppedListReorder.ViewModels;
 using Xamarin.Forms;
 
 namespace GrouppedListReorder
@@ -13,6 +14,7 @@ namespace GrouppedListReorder
         public MainPage()
         {
             InitializeComponent();
+            BindingContext = new MainPageViewModel();
         }
     }
 }
---
+++ GrouppedListReorder/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xamarin.CommunityToolkit.ObjectModel;
+
+namespace GrouppedListReorder.ViewModels
+{
+    public class MainPageViewModel : ObservableObject
+    {
+        private string _testProperty;
+        public string TestProperty
+        {
+            get { return _testProperty; }
+            set { SetProperty(ref _testProperty, value); }
+        }
+
+        public MainPageViewModel()
+        {
+            TestProperty = "test value 1";
+        }
+    }
+}
